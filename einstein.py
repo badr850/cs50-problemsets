d9f012221a09e5ab38baa99a4b791a0ca8cf7e2e
badr <badr20062020@gmail.com>
2025-01-07 15:05:39 +0100

    Create einstein.py

---
+++ einstein.py
@@ -0,0 +1,7 @@
+mass=input("please enter the mass in kg ")
+mass=int(mass)
+print("the energy calculated is :\n")
+E=mass*9*pow(10,16)
+print("E=",E,"J")
+
+#using the famous formula "E=mc^2" to calculate the energie in J, assuming that the user inputs the value of mass in kg.
